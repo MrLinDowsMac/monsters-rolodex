e24231c121a293b07d870db45718fc009edaf9a3
Ian Nájar <ianenri@gmail.com>
2020-01-03 03:36:05 -0700

    Updates

---
+++ precache-manifest.e27ed82ec7ab36ccec1f473c1834dfd5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95bb7870250efc0b5b48568602671e70",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5e195365818923481f7b",
+    "url": "/monsters-rolodex/static/css/main.fea74647.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "5e195365818923481f7b",
+    "url": "/monsters-rolodex/static/js/main.919362b6.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
